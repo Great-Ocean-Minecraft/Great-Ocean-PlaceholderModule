f9b8d63119bb39277d75a7fb2142c660dd69c25d
Demopans <gao.jeff3@gmail.com>
2022-05-12 13:38:57 -0400

    First commit
    Signed-off-by: Demopans <gao.jeff3@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,20 @@
 import org.gradle.jvm.toolchain.JavaLanguageVersion.of
 
 plugins {
-  java
-  kotlin("jvm") version "1.6.20"
+	java
+	kotlin("jvm") version "1.6.20"
 	id("io.papermc.paperweight.userdev") version "1.3.5"
-  id("com.github.johnrengelman.shadow") version "7.1.2"
+	id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 repositories {
-
+	maven { url 'https://jitpack.io' }
 }
 
 val minecraftVersion = "1.18.2-R0.1-SNAPSHOT"
 
 dependencies {
-
+	implementation 'com.github.Great-Oceans:Great-Oceans-Core:main'
 }
 
 tasks {
